8897b561762d220786147edf7016163cbe0e24ad
Miles McBain <miles.mcbain@gmail.com>
2018-11-21 12:45:26 +1000

    add functions for mercator extent checking and cropping

--- R/mercator.R
+++ R/mercator.R
@@ -36,3 +36,32 @@ merc_to_lonlat <- function(xy) {
               degrees(((pi*0.5) - 2.0 * atan(exp(-xy[, 2] / A)))))
   xy
 }
+
+##' Are points in meters within Mercator extent?
+##'
+##' When doing maths with Mercator coordinates in m, you can end up outside the
+##' Mercator extent with an undefined coordinate. This function returns true if
+##' all xy lie withib the Mercator extent.
+##' @title within_mercator_extent
+##' @param xy a matrix of Mercator xy coordinates.
+##' @return TRUE or FALSE
+##' @export
+within_merc_extent <- function(xy){
+  all(xy <= MAXEXTENT) & all(xy >= -MAXEXTENT)
+}
+
+##' Truncate coordinate to Mercator extent.
+##'
+##' If a point in m lies outside the Mercator extent, this function can be used
+##' to truncate it to the boundary of the extent.
+##' 
+##' @title merc_truncate
+##' @param xy a matrix of Mercator XY points.
+##' @return a matrix of XY points.
+##' @export
+merc_truncate <- function(xy){
+  xy[, 1] <- sm_clamp(xy[, 1], -MAXEXTENT, MAXEXTENT)
+  xy[, 2] <- sm_clamp(xy[, 2], -MAXEXTENT, MAXEXTENT)
+  xy
+}
+
