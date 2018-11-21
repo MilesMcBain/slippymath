b2694a31949417b30dbc069911a60dda7f4fdcff
Miles McBain <miles.mcbain@gmail.com>
2018-11-21 11:03:43 +1000

    add coord conv test shell

---
+++ tests/testthat/test_merc_latlon_conv.R
@@ -0,0 +1,9 @@
+context("Test conversion of lonlat to mercator")
+
+test_that("conversion functions work", {
+  uluru_merc <- c(14586472.958481, -2918162.223463)
+  uluru_lonlat <- c(131.0325162, -25.3448562)
+
+  
+
+})
