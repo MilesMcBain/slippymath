82e6f73fd935c3d887041d8f98f92cf01287450e
Miles McBain <miles.mcbain@gmail.com>
2018-11-21 11:08:21 +1000

    test conversion functions

--- tests/testthat/test_merc_latlon_conv.R
+++ tests/testthat/test_merc_latlon_conv.R
@@ -1,9 +1,9 @@
 context("Test conversion of lonlat to mercator")
 
 test_that("conversion functions work", {
-  uluru_merc <- c(14586472.958481, -2918162.223463)
-  uluru_lonlat <- c(131.0325162, -25.3448562)
+  uluru_merc <- matrix(c(14586472.958481, -2918162.223463), nrow = 1)
+  uluru_lonlat <- matrix(c(131.0325162, -25.3448562), nrow = 1)
 
-  
-
+  expect_true(all.equal(merc_to_lonlat(uluru_merc), uluru_lonlat))
+  expect_true(all.equal(lonlat_to_merc(uluru_lonlat), uluru_merc))
 })
